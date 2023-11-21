f69c6d20ac0bc7232743bcabdf365a85a8fa6c25
Charlese Muchmore <charlymuchmore@gmail.com>
2023-11-21 10:46:13 -0800

    copied lab5 to lab6 folder

---
+++ lab6/MMABooksRESTAPI/Program.cs
@@ -0,0 +1,43 @@
+using MMABooksEFClasses.MODELS;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Add cors policy - in a production app lock this down!
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+            .WithMethods("POST", "PUT", "DELETE", "GET", "OPTIONS")
+            .AllowAnyHeader();
+        });
+});
+// Adding the dbContext to the service
+builder.Services.AddDbContext<MMABOOKSCONTEXT>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// app.UseHttpsRedirection();
+
+// enables the cors policy
+app.UseCors();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
