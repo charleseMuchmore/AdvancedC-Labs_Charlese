9d9f2b026ca14a062b008d448767d84f69eb3870
Charlese Muchmore <charlymuchmore@gmail.com>
2023-12-05 15:37:12 -0800

    finished restapi stuff

---
+++ Final/RestApi/Controllers/AppUserController.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FinalProject.Models;
+
+namespace RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppUserController : Controller
+    {
+        private readonly BitsContext _context;
+        public AppUserController(BitsContext context)
+        {
+            _context = context;
+        }
+        // GET: api/AppUsers
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetAppUsers()
+        {
+            if (_context.AppUsers == null)
+            {
+                return NotFound();
+            }
+            return await _context.AppUsers.ToListAsync();
+        }
+
+        // GET: api/AppUsers/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetAppUser(int id)
+        {
+            if (_context.AppUsers == null)
+            {
+                return NotFound();
+            }
+            var appUser = await _context.AppUsers.FindAsync(id);
+
+            if (appUser == null)
+            {
+                return NotFound();
+            }
+
+            return appUser;
+        }
+
+        // PUT: api/AppUsers/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAppUser(int id, AppUser appUser)
+        {
+            if (id != appUser.AppUserId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(appUser).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/AppUsers
+        [HttpPost]
+        public async Task<ActionResult<AppUser>> PostUser(AppUser appUser)
+        {
+            if (_context.AppUsers == null)
+            {
+                return Problem("Entity set 'bits.AppUser'  is null.");
+            }
+            _context.AppUsers.Add(appUser);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAppUser", new { id = appUser.AppUserId }, appUser);
+        }
+        // DELETE: api/AppUsers/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAppUser(int id)
+        {
+            if (_context.AppUsers == null)
+            {
+                return NotFound();
+            }
+            var appUser = await _context.AppUsers.FindAsync(id);
+            if (appUser == null)
+            {
+                return NotFound();
+            }
+
+            _context.AppUsers.Remove(appUser);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool UserExists(int id)
+        {
+            return (_context.AppUsers?.Any(e => e.AppUserId == id)).GetValueOrDefault();
+        }
+    }
+}
+
+
